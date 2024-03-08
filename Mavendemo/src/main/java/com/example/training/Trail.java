64f3aef9632d6e2609797f7fce01ed79a453f333
sukanth <sukanthj11@gmail.com>
2024-03-08 16:29:27 +0530

    maven commited

---
+++ Mavendemo/src/main/java/com/example/training/Trail.java
@@ -0,0 +1,8 @@
+package com.example.training;
+
+public class Trail {
+    public static void main(String[] args) {
+        System.out.println("hii");
+        System.out.println("good morning");
+    }
+}
